a6b63b891100ab7cc896d347a921bf94386320e7
jnKav <162437560+jnKav@users.noreply.github.com>
2024-06-15 14:48:23 +0100

    Modification des pages index et contact

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
+        $mail_to = "info@sun-digital.org";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
@@ -14,7 +14,7 @@
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Veuillez remplir le formulaire er réessayer.";
             exit;
         }
         
@@ -32,17 +32,17 @@
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Merci! Votre message a été envoyé.";
         } else {
             # Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
+            echo "Oops! Un problème s'est produit, nous n'avons pas pu envoyer votre message.";
         }
 
     } else {
         # Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Il y a eu un problème avec votre soumission, veuillez réessayer.";
     }
 
 ?>
